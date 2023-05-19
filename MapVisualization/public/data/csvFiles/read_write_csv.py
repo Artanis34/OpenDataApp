d3033ecafca9a905284251ec90e6ecd26c4cf56b
Julien <julienchopin1@gmail.com>
2023-05-19 15:41:57 +0200

    status geoData

--- MapVisualization/public/data/csvFiles/read_write_csv.py
+++ MapVisualization/public/data/csvFiles/read_write_csv.py
@@ -6,15 +6,18 @@ import math
 # Set the variable csv_file to the file you want to read
 csv_file = "dienststellen_actualdate.csv"
 
-# Get the csv_file path (should always be in .../OpenDataApp/csvFiles/...)
-csv_path = os.path.join(os.getcwd(), "csvFiles", csv_file)
+# Get the current directory (where the Python file is located)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Get the csv_file path (in the same directory as the Python file)
+csv_path = os.path.join(current_dir, csv_file)
 
 # Read the CSV file and skip the first 6 lines, specifying the separator as ';'
 df_from_csv = pd.read_csv(csv_path, sep=';', skiprows=6, low_memory=False)
 
 # Get selected columns for the GeoJSON properties and rename them
-selected_columns = ['BEZEICHNUNG_OFFIZIELL', 'E_WGS84', 'N_WGS84']
-column_names = ['Name', 'Longitude', 'Latitude']
+selected_columns = ['STATUS', 'BEZEICHNUNG_OFFIZIELL', 'E_WGS84', 'N_WGS84']
+column_names = ['Status', 'Name', 'Longitude', 'Latitude']
 df_selected = df_from_csv[selected_columns].rename(columns=dict(zip(selected_columns, column_names)))
 
 # Replace NaN values with None
@@ -41,9 +44,11 @@ for index, row in df_selected.iterrows():
 # Create a GeoJSON feature collection
 feature_collection = geojson.FeatureCollection(features)
 
-# Save the GeoJSON feature collection to a file
+
+# Set the output GeoJSON file path to the parent directory of the current directory
 geojson_file = "geoData.geojson"
-geojson_path = os.path.join(os.getcwd(), geojson_file)
+output_dir = os.path.dirname(current_dir)
+geojson_path = os.path.join(output_dir, geojson_file)
 
 with open(geojson_path, 'w') as geojson_file:
     geojson.dump(feature_collection, geojson_file)
