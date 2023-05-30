fde8bf9b5575d75e15a10f812e39f9d0999abcb2
Julien <julienchopin1@gmail.com>
2023-05-30 20:19:36 +0200

    fix status

--- MapVisualization/public/data/csvFiles/select_desired_data_csv.py
+++ MapVisualization/public/data/csvFiles/select_desired_data_csv.py
@@ -124,6 +124,9 @@ try:
 
     # Update status to 0 for empty rows
     #merged_df.loc[merged_df.isnull().any(axis=1), 'Status'] = 0
+    # Update status to 0 for rows with empty or null values in any column, except 'Bezeichnung'
+    columns_to_check = merged_df.columns.drop('Bezeichung')
+    merged_df.loc[merged_df[columns_to_check].isnull().any(axis=1), 'Status'] = 0
 
     # Create a new CSV file with the selected columns (with error handling)
     try:
